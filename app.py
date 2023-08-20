eaca12f3d8842dd5823d1bb3dad81d19110ce2e9
Omar-Ahmed-Dt <omarahmed9113@gmail.com>
2023-08-20 20:56:54 +0300

    Add flask hello world application, test cases and dockerfile

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, world!"
+
+if __name__ == "__main__":
+    # Get the port from the environment variable, default to 5000
+    port = int(os.environ.get("PORT", 5000))
+
+    # Run the app with the specified settings
+    app.run(debug=True, host="0.0.0.0", port=port)
